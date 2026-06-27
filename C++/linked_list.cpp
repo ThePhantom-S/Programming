ead67af94686b7b6e8793bbe45eb77d3689eaf21
Harikrishnan <rharikrishnan8726@gmail.com>
2026-06-27 22:35:02 +0530

    Cyclic-Linked committing

--- C++/linked_list.cpp
+++ C++/linked_list.cpp
@@ -48,6 +48,7 @@ int main(){
 */
 
 //Doubly Linked List
+/*
 typedef struct Node{
 	int data;
 	struct Node* next;
@@ -97,6 +98,71 @@ int main(){
 
 	traversal_forward(node1);
 	traversal_rev(node4);
+
+	return 0;
+}
+*/
+
+//Cyclic Linked-List
+
+typedef struct Node{
+	int data;
+	struct Node* next;
+	struct Node* prev;
+}Node;
+
+Node* createNode(int data){
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	if (!newNode){
+		printf("Memory Allocation Failed");
+		exit(1);
+	}
+	newNode -> data = data;
+	newNode -> next = NULL;
+	newNode -> prev = NULL;
+	return newNode;
+}
+
+void traversal(Node* node, bool isForward = true){
+	Node* head = node;
+	while(node){
+		if (isForward){
+			printf("%d -> ", node->data);
+			node = node -> next;
+		}else{
+			printf("%d -> ", node->data);
+			node = node -> prev;
+		}
+		if (node == head){
+			printf("%d \n", node -> data);
+			return;
+		}
+	}
+}
+
+int main(){
+	
+	Node* node1 = createNode(1);
+	Node* node2 = createNode(2);
+	Node* node3 = createNode(3);
+	Node* node4 = createNode(4);
+
+	node1->next = node2;
+	node2->prev = node1;
+	node2->next = node3;
+	node3->prev = node2;
+	node3->next = node4;
+	node4->prev = node3;
+	node4->next = node1;
+	node1->prev = node4;
+
+	traversal(node1);
+	traversal(node4, false);
+
+	free(node1);
+	free(node2);
+	free(node3);
+	free(node4);
 
 	return 0;
 }
